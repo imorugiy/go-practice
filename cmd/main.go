6df31d8b30127d8ba50bbc71bb7fa31c522bfdf1
imorugiy <imorugiy@gmail.com>
2022-02-19 23:19:58 -0500

    Added server & hello handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"go-practice/handlers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	l := log.New(os.Stdout, "practice-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
 }
